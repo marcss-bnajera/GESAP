d682812d12e70984b571ae696d2a450a0405073f
JackFallas <jfallas-2021655@kinal.edu.gt>
2026-03-10 09:48:24 -0600

    fix: correcciones generales de Docker, base de datos y colección de Postman

--- init-db.sh
+++ init-db.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
     CREATE DATABASE gesap_auditor_db;
 EOSQL
